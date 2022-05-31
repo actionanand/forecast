da5f6f74ded779f515e78e0126bfd03f96fb1251
AnandRaja-tech <anand.official.in@gmail.com>
2022-05-31 19:42:08 +0000

    initial git changes

--- src/tests.webpack.js
+++ src/tests.webpack.js
@@ -1,10 +1,5 @@
-// This file is an entry point for angular tests
-// Avoids some weird issues when using webpack + angular.
-
 import 'angular';
 import 'angular-mocks/angular-mocks';
 
-const context = require.context('./app', true, /\.js$/);
-
-context.keys().forEach(context);
-
+var testsContext = require.context(".", true, /.test$/);
+testsContext.keys().forEach(testsContext);
