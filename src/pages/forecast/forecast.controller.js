f6fb282b01a4f8d58afd786676780a7985161ba3
AnandRaja-tech <anand.official.in@gmail.com>
2022-06-02 09:06:40 +0000

    weather progress

--- src/pages/forecast/forecast.controller.js
+++ src/pages/forecast/forecast.controller.js
@@ -6,16 +6,18 @@ export default class ForecastController {
     this.cityName = this.forecastCityServ.city;
     console.log(this.cityName)
 
-    this.url = 'https://api.openweathermap.org/data/2.5/weather';
-    // this.url = 'https://api.github.com/users/fikkatra';
+    // this.weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    // this.sapleGithubUrl = 'https://api.github.com/users/fikkatra';
+    this.forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast';
     this.appId = 'a2faa5d1f516e9f9615640d8abd37bef';
+    this.units = 'metric'
 
     this.weatherResult = '';
 
 
     // $http({
     //   method: 'GET',
-    //   url: this.url,
+    //   url: this.forecastUrl,
     //   params: {q: this.cityName, cnt: 2, APPID: this.appId}
     // }).then(function(resp) {
     //   console.log(resp.data);
@@ -24,16 +26,20 @@ export default class ForecastController {
     // })
 
 
-    $resource(this.url).get({q: this.cityName, cnt: 5, APPID: this.appId}).$promise.then((resp) => {
-      // console.log(resp);
+    $resource(this.forecastUrl).get({q: this.cityName, cnt: 10, units: this.units, APPID: this.appId}).$promise.then((resp) => {
+      console.log(resp);
       this.weatherResult = resp;
-      console.log(new Date(this.weatherResult.dt * 1000));
+      // console.log(new Date(this.weatherResult.dt * 1000));
     }, (err) => {
       console.log(err);
     });
 
-  }
+    this.convertToDate = function(dt) { 
+      return new Date(dt * 1000);
+    };
 
+  }
+  
 }
 
 ForecastController.$inject = ['$resource', '$http', 'forecastCity'];
